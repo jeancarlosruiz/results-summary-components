9d51fb0097aa53863e6bd5c59a13b72ddd16175f
Jean Carlos Ruiz <66891780+jeancarlosruiz@users.noreply.github.com>
2023-03-17 14:52:55 -0400

    gh-pages added and images root fixed

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -2,9 +2,36 @@ import React from "react";
 import SummaryItem from "./SummaryItem";
 import Button from "./Button";
 import styles from "./Summary.module.css";
-import data from "../data.json";
+// import data from "../data.json";
 
 function Summary() {
+  const data = [
+    {
+      category: "Reaction",
+      score: 80,
+      iconImg: "/icon-reaction.svg",
+      iconAlt: "Thunder icon",
+    },
+    {
+      category: "Memory",
+      score: 92,
+      iconImg: "/icon-memory.svg",
+      iconAlt: "Brain icon",
+    },
+    {
+      category: "Verbal",
+      score: 61,
+      iconImg: "/icon-verbal.svg",
+      iconAlt: "Communication icon",
+    },
+    {
+      category: "Visual",
+      score: 72,
+      iconImg: "/icon-visual.svg",
+      iconAlt: "Eye icon",
+    },
+  ];
+
   return (
     <div className={styles.summary}>
       <h2 className={styles.sectionTitle}>Summary</h2>
